fe266ef27f749bb1b9e91c071aa4e90ce45eeb22
truscoutjohn <sashatkachenkobusiness@gmail.com>
2022-12-07 20:03:05 +0200

    feat: lesson2 task counter-redux

---
+++ lesson2/counter-redux/src/index.js
@@ -0,0 +1,25 @@
+import { createStore } from 'redux';
+
+const INCREMENT = 'COUNTER/INCREMENT';
+const DECREMENT = 'COUNTER/DECREMENT';
+
+export const increment = () => ({
+  type: INCREMENT,
+});
+
+export const decrement = () => ({
+  type: DECREMENT,
+});
+
+const counterReducer = (state = 0, action) => {
+  switch (action.type) {
+    case INCREMENT:
+      return state + 1;
+    case DECREMENT:
+      return state - 1;
+    default:
+      return state;
+  }
+};
+
+export default createStore(counterReducer);
