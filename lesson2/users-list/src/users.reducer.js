0148d3183c2aabca02922c7bbb3de80dfbb497d3
truscoutjohn <sashatkachenkobusiness@gmail.com>
2022-12-08 13:29:30 +0200

    refactor: rename variable

--- lesson2/users-list/src/users.reducer.js
+++ lesson2/users-list/src/users.reducer.js
@@ -1,11 +1,11 @@
 import { USER_ADD, USER_DELETE } from './users.actions.js';
 
-export const userReducer = (state = { userList: [] }, action) => {
+export const userReducer = (state = { usersList: [] }, action) => {
   switch (action.type) {
     case USER_ADD:
-      return { ...state, userList: state.userList.concat(action.payload) };
+      return { ...state, usersList: state.usersList.concat(action.payload) };
     case USER_DELETE:
-      return { ...state, userList: state.userList.filter(user => user.id !== action.payload.id) };
+      return { ...state, usersList: state.usersList.filter(user => user.id !== action.payload.id) };
     default:
       return state;
   }
