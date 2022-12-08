fc9af2c239a1d88e1fbc88d9dd54d5ccd17aa53f
truscoutjohn <sashatkachenkobusiness@gmail.com>
2022-12-08 12:24:51 +0200

    feat: lesson 2 task counter history2

--- lesson2/counter-history/src/store.js
+++ lesson2/counter-history/src/store.js
@@ -14,9 +14,9 @@ const initialState = {
 const counterReducer = (state = initialState, action) => {
   switch (action.type) {
     case DECREMENT:
-      return { ...state, history: [...state.history, -1] };
+      return { ...state, history: state.history.concat(-1) };
     case INCREMENT:
-      return { ...state, history: [...state.history, 1] };
+      return { ...state, history: state.history.concat(1) };
     case RESET:
       return { ...state, history: [] };
     default:
